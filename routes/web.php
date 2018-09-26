d3bfd47b49a7d945b95d33869dea26d345ce95ef
Māris V <vilskersts.maris@gmail.com>
2018-09-26 18:10:38 +0300

    Added auth middleware to routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-\Illuminate\Support\Facades\Route::group( [ 'middleware' => 'web' ], function () {
+\Illuminate\Support\Facades\Route::group( [ 'middleware' => [ 'web', 'auth' ] ], function () {
     \Illuminate\Support\Facades\Route::get( 'translations/{group?}', 'Phycon\Translations\Controllers\TranslationsController@index' )->name( 'translations.index' );
     \Illuminate\Support\Facades\Route::post( 'translations/filter', 'Phycon\Translations\Controllers\TranslationsController@filter' )->name( 'translations.filter' );
     \Illuminate\Support\Facades\Route::post( 'translations/{group?}', 'Phycon\Translations\Controllers\TranslationsController@update' );
